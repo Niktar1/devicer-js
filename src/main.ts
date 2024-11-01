815f710af12fccc6df768e4e68b1754841e0fc2f
Nik <com.nik570@gmail.com>
2024-11-02 01:01:41 +0200

    added users

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
-async function bootstrap() {
+async function start() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 5000;
+
+  const config = new DocumentBuilder()
+    .setTitle('BACKEND with Node.js. & Nest js')
+    .setDescription('REST API Documentation')
+    .setVersion('1.0.0')
+    .addTag('Niktar1')
+    .build()
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/api/docs', app, document)
+
+  await app.listen(PORT);
 }
-bootstrap();
+start();
